35b1a581b303dc2d79cb096721d1845aa4c4eee9
Bryan <bryankeefecresswell@gmail.com>
2017-06-15 20:32:47 +0800

    updated with filter functionality and marker filtering

--- js/gMaps.js
+++ js/gMaps.js
@@ -1,6 +1,6 @@
 var map;
 var marker, i;
-var markersArray = [];
+window.markersArray = {};
 var currentLocation = {lat: 1.28631490, lng: 103.827403};
 var stylesArray =[
 	{
@@ -111,43 +111,43 @@ var stylesArray =[
 		]
 	}
 ];
-var markers = {
-	"Tiong Bahru Station (EWL)": {lat:1.28498, lng:103.82283},
-	"Redhill Station (EWL)": {lat:1.289521, lng:103.816951},
-	"Queenstown Station (EWL)": {lat:1.295428, lng:103.805623},
-	"Outram Park Station (EWL)": {lat:1.281395, lng:103.838932},
-	"Tanjong Pagar Station (EWL)": {lat:1.276407, lng:103.846700},
-	"Commonwealth Station (EWL)": {lat:1.302320, lng: 103.798039},
-	"Buona Vista Station (EWL)": {lat:1.307382, lng: 103.790325},
-	"Dover Station (EWL)": {lat:1.311278, lng:103.778552},
-	"Clementi Station (EWL)": {lat:1.315325, lng:103.764659},
-	"Jurong East Station (EWL)": {lat:1.333133, lng:103.742213},
-	"Chinese Garden Station (EWL)": {lat:1.342491, lng:103.732606},
-	"Lakeside Station (EWL)": {lat:1.344155,lng:103.720819},
-	"Boon Lay Station (EWL)": {lat:1.338659, lng:103.705894},
-	"Pioneer Station (EWL)": {lat: 1.337461, lng:103.696780},
-	"Joo Koon Station (EWL)": {lat: 1.327817, lng: 103.678311},
-	"Raffles Place Station (EWL)": {lat:1.283879, lng:103.851012},
-	"City Hall Station (EWL)": {lat:1.293241, lng:103.852166},
-	"Bugis Station (EWL)": {lat:1.300910, lng:103.856685},
-	"Lavender Station (EWL)": {lat:1.307137, lng:103.863122},
-	"Aljunied Station (EWL)": {lat:1.316419, lng:103.882856},
-	"Paya Lebar Station (EWL)": {lat:1.317792, lng:103.893756},
-	"Eunos Station (EWL)": {lat: 1.319766, lng:103.902597},
-	"Kembangan Station (EWL)": {lat:1.321053,lng:103.912811},
-	"Bedok Station (EWL)": {lat:1.324056, lng:103.930195},
-	"Tanah Merah Station (EWL)": {lat: 1.327402, lng:103.946374},
-	"Expo Station (EWL)": {lat:1.334953, lng:103.961823},
-	"Simei Station (EWL)": {lat: 1.343234, lng:103.953412},
-	"Tampines Station (EWL)": {lat: 1.353071, lng: 103.945246},
-	"Pasir Ris Station (EWL)": {lat: 1.373077, lng:103.949436},
-	"Changi Airport Station (EWL)": {lat:1.357558, lng:103.988891}
-};
+var markers = [
+	{name:"Tiong Bahru Station (EWL)", position: {lat:1.28498, lng:103.82283}},
+	{name:"Redhill Station (EWL)", position: {lat:1.289521, lng:103.816951}},
+	{name:"Queenstown Station (EWL)", position: {lat:1.295428, lng:103.805623}},
+	{name:"Outram Park Station (EWL)", position: {lat:1.281395, lng:103.838932}},
+	{name:"Tanjong Pagar Station (EWL)", position: {lat:1.276407, lng:103.846700}},
+	{name:"Commonwealth Station (EWL)", position: {lat:1.302320, lng: 103.798039}},
+	{name:"Buona Vista Station (EWL)", position: {lat:1.307382, lng: 103.790325}},
+	{name:"Dover Station (EWL)", position: {lat:1.311278, lng:103.778552}},
+	{name:"Clementi Station (EWL)", position: {lat:1.315325, lng:103.764659}},
+	{name:"Jurong East Station (EWL)", position: {lat:1.333133, lng:103.742213}},
+	{name:"Chinese Garden Station (EWL)", position: {lat:1.342491, lng:103.732606}},
+	{name:"Lakeside Station (EWL)", position: {lat:1.344155,lng:103.720819}},
+	{name:"Boon Lay Station (EWL)", position: {lat:1.338659, lng:103.705894}},
+	{name:"Pioneer Station (EWL)", position: {lat: 1.337461, lng:103.696780}},
+	{name:"Joo Koon Station (EWL)", position: {lat: 1.327817, lng: 103.678311}},
+	{name:"Raffles Place Station (EWL)", position: {lat:1.283879, lng:103.851012}},
+	{name:"City Hall Station (EWL)", position: {lat:1.293241, lng:103.852166}},
+	{name:"Bugis Station (EWL)", position: {lat:1.300910, lng:103.856685}},
+	{name:"Lavender Station (EWL)", position: {lat:1.307137, lng:103.863122}},
+	{name:"Aljunied Station (EWL)", position: {lat:1.316419, lng:103.882856}},
+	{name:"Paya Lebar Station (EWL)", position: {lat:1.317792, lng:103.893756}},
+	{name:"Eunos Station (EWL)", position: {lat: 1.319766, lng:103.902597}},
+	{name:"Kembangan Station (EWL)", position: {lat:1.321053,lng:103.912811}},
+	{name:"Bedok Station (EWL)", position: {lat:1.324056, lng:103.930195}},
+	{name:"Tanah Merah Station (EWL)", position: {lat: 1.327402, lng:103.946374}},
+	{name:"Expo Station (EWL)", position: {lat:1.334953, lng:103.961823}},
+	{name:"Simei Station (EWL)", position: {lat: 1.343234, lng:103.953412}},
+	{name:"Tampines Station (EWL)", position: {lat: 1.353071, lng: 103.945246}},
+	{name:"Pasir Ris Station (EWL)", position: {lat: 1.373077, lng:103.949436}},
+	{name:"Changi Airport Station (EWL)", position: {lat:1.357558, lng:103.988891}}
+];
 function initMap() {
 	var styledMapType = new google.maps.StyledMapType(stylesArray, {name: "Dark Mode"});
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: currentLocation,
-		zoom: 14,
+		zoom: 12,
 		mapTypeControlOptions: {
 			mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
 		}
@@ -160,18 +160,18 @@ function initMap() {
 	}
 	var currentMarker = null;
 	var infoWindow = new google.maps.InfoWindow();
-	for (var name in markers) {
+
+	for (var i = 0; i < markers.length; i ++) {
 		marker = new google.maps.Marker({
-			position: markers[name],
+			position: markers[i].position,
 			map: map,
 			animation: google.maps.Animation.DROP
 		});
-		markersArray.push({name:marker});
+		var name = markers[i].name;
 		google.maps.event.addListener(marker, 'click', (function(marker, name) {
 			return function() {
 				if (currentMarker) currentMarker.setAnimation(null);
 				currentMarker = marker;
-				console.log(currentMarker);
 				marker.setAnimation(google.maps.Animation.BOUNCE);
 				map.setCenter(marker.getPosition());
 				infoWindow.setContent(name);
@@ -184,6 +184,7 @@ function initMap() {
 				marker.setAnimation(null);
 			}
 		})(marker));
+		markersArray[name] = marker;
 	}
 }
 
@@ -196,21 +197,15 @@ function toggleBounce() {
 }
 	
 
-// // Foursquare AJAX
-
-// var CLIENT_ID = '2EV04IVMAWUQ1FQ15YCALQWPJDHQDDHYZ1ERBNICRYHS4QEB'
-// var CLIENT_SECRET = '5NWZMBYFJAAODSXJ1SVINVPUGHFYOW4FJDIEYYZQW2L0Y0IJ'
+// // AJAX for MRT API
 // $.ajax({
-// 	url: 'https://api.foursquare.com/v2/venues/search',
+// 	url: 'https://mrtapi.com/api/v1.1/getTrainArrival&TIB',
 // 	dataType: 'json',
-// 	data: 'limit=10' + 
-// 		'&ll1.35,103.8' + 
-// 		'&client_id = ' + CLIENT_ID +
-// 		'&client_secret = ' + CLIENT_SECRET +
-// 		'&v = 20170613',
-// 	async: true,
-// 	success: function(data)
-// })
+// 	data: 'stnCode:TIB'
+// 	success: function(data) {
+// 		alert("lol");
+// 	}
+// });
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,79 @@
-// KNOCKOUT
-stationList = [
-		{name: "Tiong Bahru Station (EWL)"},
-		{name: "Redhill Station (EWL)"},
-		{name: "Queenstown Station (EWL)"},
-		{name: "Outram Park Station (EWL)"},
-		{name: "Tanjong Pagar Station (EWL)"},
-		{name: "Commonwealth Station (EWL)"},
-		{name: "Buona Vista Station (EWL)"},
-		{name: "Dover Station (EWL)"},
-		{name: "Clementi Station (EWL)"},
-		{name: "Jurong East Station (EWL)"},
-		{name: "Chinese Garden Station (EWL)"},
-		{name: "Lakeside Station (EWL)"},
-		{name: "Boon Lay Station (EWL)"},
-		{name: "Pioneer Station (EWL)"},
-		{name: "Joo Koon Station (EWL)"},
-		{name: "Raffles Place Station (EWL)"},
-		{name: "City Hall Station (EWL)"},
-		{name: "Bugis Station (EWL)"},
-		{name: "Lavender Station (EWL)"},
-		{name: "Aljunied Station (EWL)"},
-		{name: "Paya Lebar Station (EWL)"},
-		{name: "Eunos Station (EWL)"},
-		{name: "Kembangan Station (EWL)"},
-		{name: "Bedok Station (EWL)"},
-		{name: "Tanah Merah Station (EWL)"},
-		{name: "Expo Station (EWL)"},
-		{name: "Simei Station (EWL)"},
-		{name: "Tampines Station (EWL)"},
-		{name: "Pasir Ris Station (EWL)"},
-		{name: "Changi Airport Station (EWL)"}
-	];
-
+// Knockout
+var stationList = [
+		{name: "Tiong Bahru Station (EWL)", marker: markersArray['Tiong Bahru Station (EWL)']},
+		{name: "Redhill Station (EWL)", marker: markersArray['Redhill Station (EWL)']},
+		{name: "Queenstown Station (EWL)", marker: markersArray['Queenstown Station (EWL)']},
+		{name: "Outram Park Station (EWL)", marker: markersArray['Outram Park Station (EWL)']},
+		{name: "Tanjong Pagar Station (EWL)", marker: markersArray['Tanjong Pagar Station (EWL)']},
+		{name: "Commonwealth Station (EWL)", marker: markersArray['Commonwealth Station (EWL)']},
+		{name: "Buona Vista Station (EWL)", marker: markersArray['Buona Vista Station (EWL)']},
+		{name: "Dover Station (EWL)", marker: markersArray['Dover Station (EWL)']},
+		{name: "Clementi Station (EWL)", marker: markersArray['Clementi Station (EWL)']},
+		{name: "Jurong East Station (EWL)", marker: markersArray['Jurong East Station (EWL)']},
+		{name: "Chinese Garden Station (EWL)", marker: markersArray['Chinese Garden Station (EWL)']},
+		{name: "Lakeside Station (EWL)", marker: markersArray['Lakeside Station (EWL)']},
+		{name: "Boon Lay Station (EWL)", marker: markersArray['Boon Lay Station (EWL)']},
+		{name: "Pioneer Station (EWL)", marker: markersArray['Pioneer Station (EWL)']},
+		{name: "Joo Koon Station (EWL)", marker: markersArray['Joo Koon Station (EWL)']},
+		{name: "Raffles Place Station (EWL)", marker: markersArray['Raffles Place Station (EWL)']},
+		{name: "City Hall Station (EWL)", marker: markersArray['City Hall Station (EWL)']},
+		{name: "Bugis Station (EWL)", marker: markersArray['Bugis Station (EWL)']},
+		{name: "Lavender Station (EWL)", marker: markersArray['Lavender Station (EWL)']},
+		{name: "Aljunied Station (EWL)", marker: markersArray['Aljunied Station (EWL)']},
+		{name: "Paya Lebar Station (EWL)", marker: markersArray['Paya Lebar Station (EWL)']},
+		{name: "Eunos Station (EWL)", marker: markersArray['Eunos Station (EWL)']},
+		{name: "Kembangan Station (EWL)", marker: markersArray['Kembangan Station (EWL)']},
+		{name: "Bedok Station (EWL)", marker: markersArray['Bedok Station (EWL)']},
+		{name: "Tanah Merah Station (EWL)", marker: markersArray['Tanah Merah Station (EWL)']},
+		{name: "Expo Station (EWL)", marker: markersArray['Expo Station (EWL)']},
+		{name: "Simei Station (EWL)", marker: markersArray['Simei Station (EWL)']},
+		{name: "Tampines Station (EWL)", marker: markersArray['Tampines Station (EWL)']},
+		{name: "Pasir Ris Station (EWL)", marker: markersArray['Pasir Ris Station (EWL)']},
+		{name: "Changi Airport Station (EWL)", marker: markersArray['Changi Airport Station (EWL)']}
+];
 // Knockout Model (Station List)
+/**
+* @description Creates the Stations object
+* @constructor
+* @param {string} title - The title of the book
+* @param {string} author - The author of the book
+*/
 var Stations = function(data) {
 	this.name = ko.observable(data.name);
+	this.marker = ko.observable(data.marker);
 };
 
 // Knockout ViewModel
 var neighbourhoodMapViewModel = function() {
 	var self = this;
 	this.locationList = ko.observableArray([]);
+	this.holdingList = ko.observableArray([]);
 	stationList.forEach(function(stationItem) {
 		self.locationList.push(new Stations(stationItem));
 	});
+
 	this.currentLocation = ko.observable(this.locationList()[0]);
 	this.showMarkerInfo = function(name) {
-		google.maps.event.trigger(markersArray.name, 'click');
+		var trigger = name.name()
+		google.maps.event.trigger(markersArray[trigger], 'click');
 	};
-	// Filter Function
+	// Filter Function (includes Marker functions that are handled via Knockout's computed observable)
 	this.filter = ko.observable("");
 	this.filterSearch = ko.computed(function() {
 		var filter = self.filter().toLowerCase();
 		if (!filter) {
+			for (var i = 0; i < self.locationList().length; i ++) {
+				self.locationList()[i].marker().setVisible(true);
+			}
 			return self.locationList();
 		} else {
 			return ko.utils.arrayFilter(self.locationList(), function(item) {
-				var result = stringStartsWith(item.name().toLowerCase(), self.filter().toLowerCase());
-				console.log(item.name().index);
+				var result = stringStartsWith(item.name().toLowerCase(), filter);
+				if (result) {
+					item.marker().setVisible(true);
+				} else {
+					item.marker().setVisible(false);
+				}
 				return result;
 			});
 		}
